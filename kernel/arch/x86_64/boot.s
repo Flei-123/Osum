a712375b29e494d3c65e60b173c6defed032dfe4
Justin <justin@fleitec.com>
2026-08-29 19:47:17 +0000

    ASYNC 1/n: die Auftragsschicht steht -- kernel/async.fi (Auftragstafel mit zaehlender Referenz aufs Ziel, Fertigmeldungsring je Prozess, Abbruch, Prozesstod), sechs Aufrufe ab 1980, vier Arbeitsfaeden, poll als Auftrag. 32 + 7 Zusagen aus Ring 3, 27 aus /bin/aiot ueber die libc, drei Gegenproben. tools/async/run.sh: 108 Zusagen.

--- kernel/arch/x86_64/boot.s
+++ kernel/arch/x86_64/boot.s
@@ -53,7 +53,7 @@
     .set MB_FLAGS, 0x00000007
     .set MB_CHECK, -(MB_MAGIC + MB_FLAGS)
 
-    .set KDATA_SIZE, 0x80000            /* 512 KiB, see kstate.fi (round NETMON) */
+    .set KDATA_SIZE, 0x90000            /* 576 KiB, see kstate.fi (Runde ASYNC) */
 
     .section .multiboot, "a"
     .align 4
