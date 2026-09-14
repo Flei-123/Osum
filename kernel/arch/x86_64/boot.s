091bed778f168e968064b837b2ef1990ed2bda3c
Flei123 <justin@fleitec.com>
2026-09-14 22:24:17 +0000

    Welle 15.09.: kdata auf 0x140000, die drei Bereiche VORHER vergeben (SUSP/CRYPT/CODEC)

--- kernel/arch/x86_64/boot.s
+++ kernel/arch/x86_64/boot.s
@@ -53,7 +53,7 @@
     .set MB_FLAGS, 0x00000007
     .set MB_CHECK, -(MB_MAGIC + MB_FLAGS)
 
-    .set KDATA_SIZE, 0x110000           /* 1088 KiB, see kstate.fi (merge 14.09.2026) */
+    .set KDATA_SIZE, 0x140000           /* 1280 KiB, see kstate.fi (welle 15.09.2026) */
 
     .section .multiboot, "a"
     .align 4
